e76554197ea6f96e5efac41d37826fb1d092d92d
AntonioHincapie <marcohincapiem@gmail.com>
2022-05-12 15:15:49 -0500

    Task 3 completed

--- test.js
+++ test.js
@@ -29,3 +29,43 @@ test("Hello to be olleH", () => {
 test("Hi to be iH", () => {
   expect(reverseString("Hi")).toEqual("iH");
 });
+
+// Test 3 Calculator
+
+const calculator = require("./calculator.js");
+
+describe("add", () => {
+  test("1+2 to be 3", () => {
+    expect(new calculator().add(1, 2)).toBe(3);
+  });
+  test("4+5 to be 9", () => {
+    expect(new calculator().add(4, 5)).toBe(9);
+  });
+});
+
+describe("subtract", () => {
+  test("5-2 to be 3", () => {
+    expect(new calculator().subtract(5, 2)).toBe(3);
+  });
+  test("6-3 to be 3", () => {
+    expect(new calculator().subtract(6, 3)).toBe(3);
+  });
+});
+
+describe("divide", () => {
+  test("8/2 to be 4", () => {
+    expect(new calculator().divide(8, 2)).toBe(4);
+  });
+  test("6/3 to be 2", () => {
+    expect(new calculator().divide(6, 3)).toBe(2);
+  });
+});
+
+describe("multipy", () => {
+  test("5*2 to be 10", () => {
+    expect(new calculator().multiply(5, 2)).toBe(10);
+  });
+  test("6*3 to be 18", () => {
+    expect(new calculator().multiply(6, 3)).toBe(18);
+  });
+});
