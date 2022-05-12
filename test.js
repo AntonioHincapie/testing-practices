98c3ef56451743b68b0412c03dcced4a61b9b1dd
AntonioHincapie <marcohincapiem@gmail.com>
2022-05-12 14:22:33 -0500

    Task 2 completed

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+// Test 1 Length
+
 const stringLength = require("./stringLength.js");
 
 test("hello to be 5", () => {
@@ -14,4 +16,16 @@ test("Case X to be 6", () => {
 
 test("X to be 1", () => {
   expect(stringLength("X")).toEqual(1);
+});
+
+// Test 2 Reverse
+
+const reverseString = require("./reverseString.js");
+
+test("Hello to be olleH", () => {
+  expect(reverseString("Hello")).toEqual("olleH");
+});
+
+test("Hi to be iH", () => {
+  expect(reverseString("Hi")).toEqual("iH");
 });
